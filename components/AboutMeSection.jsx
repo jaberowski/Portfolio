a940c67c0a0192d4fac7e038485f943bdb422601
Jaber Fathi <jaber.programming@gmail.com>
2023-05-06 12:45:19 +0430

    change about me text

--- components/AboutMeSection.jsx
+++ components/AboutMeSection.jsx
@@ -19,25 +19,21 @@ function AboutMeSection({ setCurrentSection }) {
           width={300}
           height={300}
           className="w-64 md:w-80 2xl:w-96"
+          alt="jaber fathi image"
         />
         <div className="flex flex-col gap-5 items-center md:items-start">
           <h2 className="text-2xl lg:text-3xl 2xl:text-4xl font-bold">
             Hi, I’m Jaber. Nice to meet you.
           </h2>
           <p className="text-lg  lg:text-xl leading-8">
-            I got interested in programming a couple of years ago after my
-            previous plans failed. From the beginning of my journey, I tried to
-            learn programming on my own by reading articles and documentations,
-            and watching courses of the best instructors in the world. Even
-            though it had its difficulties, going through this process improved
-            my problem-solving skills and I believe it would help me keep up
-            with cutting-edge technologies in the future. I managed to reach
-            this point despite working a part-time job and studying for my
-            Bachelor&#39;s degree at university. I&#39;m looking for my first
-            job now that I&#39;ve decided to pursue it as a career. So, if there
-            is an opening for a passionate junior Front-end web developer with
-            good idea&#39;s who can chat about football events for hours, I
-            believe I would be a perfect match for your team.
+            I got interested in programming a couple of years ago and I&#39;m
+            enjoying coding ever since. For the last two years, reading
+            documentation and articles, doing projects, and asking around about
+            how to get my code to work have been highlights of my days. Now that
+            I&#39;ve decided to pursue it as a career, I&#39;m excited to work
+            as part of a team. So, if there is an opening for a passionate
+            junior Front-end web developer, I believe I would be a perfect match
+            for your team.
           </p>
         </div>
       </div>
